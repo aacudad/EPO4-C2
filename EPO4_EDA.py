ff1d8505666e25fa00b42c124719eb84e50e33f0
jackchen27 <jack.chen2712@gmail.com>
2023-05-19 00:37:14 +0200

    Comments en efficienter

--- EPO4_EDA.py
+++ EPO4_EDA.py
@@ -1,35 +1,35 @@
 # %%
+# Importing libraries
 import numpy as np # linear algebra
 import pandas as pd # data processing
-
+import matplotlib.pyplot as plt # plotting
 import biobss #EDA analysis
 
 import os
 import pickle
+
 from scipy import signal
 from scipy.signal import butter, iirnotch, lfilter
+import scipy.signal
+import scipy.ndimage
+import scipy.stats as stats
 
 from os import listdir
 from os.path import isfile, join, isdir
 
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import scipy.signal
-import scipy.ndimage
-import scipy.stats as stats
-# %%
-DF_feat = pd.DataFrame()
-
 #%%
+# Empty dataframe and array to append data
 DF_feat = pd.DataFrame()
 begin_l = np.empty((0,1))
 end_l = np.empty((0,1))
 index_l = np.empty((0,1))
 
+# For loop for all 15 subjects
 for i in range(16): # 16
-    subject = 'S'+str(i+2)
-    data_set_path= "C:/Users/JackC/Documents/EPO4/WESAD/WESAD/"
-    if subject != 'S12':
+    subject = 'S'+str(i+2) # Cycle through S2 to S17
+    data_set_path= "C:/Users/JackC/Documents/EPO4/WESAD/WESAD/" # Folder path
+    if subject != 'S12': # Skip S12, because it does nog exist
         class read_data_of_one_subject:
             """Read data from WESAD dataset"""
             def __init__(self, path, subject):
@@ -77,14 +77,16 @@ for i in range(16): # 16
         baseline = np.asarray([idx for idx,val in enumerate(labels) if val == 2])
         #plt.plot(labels)
 
-        eda_base=chest_data_dict['EDA'][baseline,0]
-        sampling_rate=700
+        eda_base=chest_data_dict['EDA'][baseline,0] # Select the EDA data
+
         # cut a smaller window
+        sampling_rate=700
         eda=eda_base#[400*sampling_rate:420*sampling_rate]
 
         t=np.arange(0,eda.size*(1/sampling_rate),(1/sampling_rate))
         t1=t[:eda.size]
 
+        # Plot the original sampled data
         #plt.figure(figsize=(12,4))
         #plt.plot(t1,eda)
         #plt.xlabel('$Time (s)$') 
@@ -94,31 +96,36 @@ for i in range(16): # 16
         #Filtered signal
         eda_f = biobss.edatools.filter_eda(eda, sampling_rate)
 
+        # Plot the filtered data
         #plt.figure(figsize=(12,4))
         #plt.plot(t,eda_f)
         #plt.xlabel('$Time (s)$') 
         #plt.ylabel('$EDA$')
-
+        
+        # Decompose the signal in a tonic and phasic part
         eda = biobss.edatools.eda_decompose(eda_f, sampling_rate=sampling_rate)
         tonic = eda.iloc[:,0]
         phasic = eda.iloc[:,1]
         #print(eda)
 
+        # Zero pad the signal to avoid errors
         zero_pad = 55*sampling_rate-1
         phasic = np.pad(phasic, (zero_pad, 0))
-        phasic = np.insert(phasic,0,1)
+        phasic = np.insert(phasic,0,0)
         phasic = np.pad(phasic, (5, 0))
         t2 = np.arange(0,phasic.size*(1/sampling_rate),(1/sampling_rate))
         #print(phasic)
         #print(phasic.size)
         #print(t2.size)
 
+        # Plot the zero padded signal
         #plt.figure(figsize=(12,4))
         #plt.plot(t2,phasic,label='phasic')
         ##plt.plot(t2,tonic,label='tonic')
         #plt.xlabel('$Time (s)$') 
         #plt.ylabel('$EDA$')
 
+        # Peak detection
         peaks, properties = scipy.signal.find_peaks(phasic)
         heights, left_bases, right_bases = scipy.signal.peak_prominences(phasic, peaks)
         widths, width_heights, left_ips, __ = scipy.signal.peak_widths(phasic, peaks, rel_height=0.9)
@@ -161,7 +168,7 @@ for i in range(16): # 16
         left_ips = left_ips[keep1]
         right_ips = right_ips[keep1]
 
-        ## plot
+        ## plot the peaks
         #plt.figure(figsize=(12,4))
         #plt.plot(t2,phasic,label='phasic')
         #plt.plot(t2[peaks],phasic[peaks],'o',label='peaks')
@@ -170,12 +177,13 @@ for i in range(16): # 16
         #plt.ylabel('$EDA$')
         #plt.legend()
 
-        ##Taking the time interval between of the cluster of peaks
-        tf = t2[peaks[1]-5*sampling_rate:peaks[-1]+5*sampling_rate]
-        phasic = phasic[peaks[1]-5*sampling_rate:peaks[-1]+5*sampling_rate]
+        ##Taking the time interval between of the cluster of peaks with 5 sec extra
+        tf = t2[peaks[0]-5*sampling_rate:peaks[-1]+5*sampling_rate] 
+        phasic = phasic[peaks[0]-5*sampling_rate:peaks[-1]+5*sampling_rate]
         #print(tf.size)
         #print(phasic.size)
-        ## plot
+
+        # plot the interval
         #plt.figure(figsize=(12,4))
         #plt.plot(tf,phasic,label='phasic')
         ## labels and titles
@@ -187,38 +195,43 @@ for i in range(16): # 16
         start = peaks[1]-5*sampling_rate
         end = peaks[-1]+5*sampling_rate
 
+        # Append all start and end indices to matrix
         begin_l = np.append(begin_l, start[np.newaxis][np.newaxis], axis = 0)
         end_l = np.append(end_l, end[np.newaxis][np.newaxis], axis = 0)
-        print(begin_l)
-        print(end_l)
+        #print(begin_l)
+        #print(end_l)
 
         #Features from EDA signal
         tonic_phase_feat = biobss.edatools.from_decomposed(phasic, tonic, sampling_rate)
-        eda_feat = biobss.edatools.from_signal(eda_f, sampling_rate)
-        eda_freq_feat = biobss.edatools.eda_freq_features(eda)
-        eda_hjorth_feat = biobss.edatools.eda_hjorth_features(eda)
-        eda_sig_feat = biobss.edatools.eda_signal_features(eda)
-        eda_stat_feat = biobss.edatools.eda_stat_features(eda)
+        #eda_feat = biobss.edatools.from_signal(eda_f, sampling_rate)
+        #eda_freq_feat = biobss.edatools.eda_freq_features(eda)
+        #eda_hjorth_feat = biobss.edatools.eda_hjorth_features(eda)
+        #eda_sig_feat = biobss.edatools.eda_signal_features(eda)
+        #eda_stat_feat = biobss.edatools.eda_stat_features(eda)
 
-        ##Features from tonic
-        tonic_feat = biobss.edatools.from_scl(tonic)
+        #Features from tonic
+        #tonic_feat = biobss.edatools.from_scl(tonic)
 
         ##Features from phasic
-        phasic_feat = biobss.edatools.from_scr(phasic)
+        #phasic_feat = biobss.edatools.from_scr(phasic)
 
+        # Convert dictionary to Dataframe
         DF_feat1 = pd.DataFrame.from_dict([tonic_phase_feat])
         #DF_feat2 = pd.DataFrame.from_dict([eda_freq_feat])
         #DF_feat3 = pd.DataFrame.from_dict([eda_sig_feat])
-        DF_feat = DF_feat.append(DF_feat1)
+        DF_feat = pd.concat([DF_feat,DF_feat1])
         #DF_feat = pd.concat([DF_feat,DF_feat2], axis = 1)
         #DF_feat = pd.concat([DF_feat,DF_feat3], axis = 1)
-        print(DF_feat)
-        
+        #print(DF_feat)
+
 #%%
+# Combine begin and end index matrices
 index_l = np.append(begin_l, end_l, axis = 1)
 print(index_l)
 
+# Fill the Nan with the median of the values of the column
 DF_feat.fillna(DF_feat.median(), inplace = True)
 print(DF_feat)
 
+# Convert to csv file
 DF_feat.to_csv("EDA_features.csv")
